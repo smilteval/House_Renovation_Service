e6f9733c2499987a8cf71fa397c1ee0d30c6d765
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-04-22 19:58:56 -0400

    Added search by specialization, updated styling of the homepage

--- pages/homepage.php
+++ pages/homepage.php
@@ -19,41 +19,106 @@
 
     <!-- My CSS -->
 
-    <link rel="stylesheet" href="styling/homepage.css" />
+    <link rel="stylesheet" href="../styling/homepage.css" />
 
     <title>Homepage</title>
 </head>
 
 <body>
-    <a href="http://localhost/house_renovation_service/pages/create_order.php" class="btn btn-primary">
-        Create an Order
-    </a>
+
+    <!-- Create an order button -->
+
+
+
+    <!-- Search fields -->
+
     <div class="container mt-4">
+
         <div class="row d-flex justify-content-center">
+
             <div class="col-md-9">
+                <div class="row d-flex justify-content-end mr-1">
+                    <a href="http://localhost/house_renovation_service/pages/create_order.php" class="btn btn-primary"
+                        id="order-button">
+                        Create an Order
+                    </a>
+                </div>
                 <div class="card p-4 mt-3">
+
                     <!-- Search bar for zipcode -->
+
                     <h3 class="heading mt-5 text-center">Search By Zipcode</h3>
                     <div class="d-flex justify-content-center px-5">
                         <div class="search">
                             <input type="number" class="search-input" placeholder="Enter your zipcode...">
-                            <a href="http://localhost/house_renovation_service/pages/zipcode_search_results.php" class="search-icon"> <i class="fa fa-search"></i> </a>
+                            <a href="http://localhost/house_renovation_service/pages/zipcode_search_results.php"
+                                class="search-icon"> <i class="fa fa-search"></i> </a>
                         </div>
                     </div>
+
                     <!-- Search bar for address  -->
+
                     <h3 class="heading mt-5 text-center">Search By Address</h3>
                     <div class="d-flex justify-content-center px-5">
                         <div class="search">
                             <input type="text" class="search-input" placeholder="Enter your address...">
-                            <a href="http://localhost/house_renovation_service/pages/address_search_results.php" class="search-icon"> <i class="fa fa-search"></i> </a>
+                            <a href="http://localhost/house_renovation_service/pages/address_search_results.php"
+                                class="search-icon"> <i class="fa fa-search"></i> </a>
                         </div>
                     </div>
+
                     <!-- Search bar for budget  -->
+
                     <h3 class="heading mt-5 text-center">Search By Budget</h3>
                     <div class="d-flex justify-content-center px-5">
                         <div class="search">
                             <input type="number" class="search-input" placeholder="Enter your budget...">
-                            <a href="http://localhost/house_renovation_service/pages/budget_search_results.php" class="search-icon"> <i class="fa fa-search"></i> </a>
+                            <a href="http://localhost/house_renovation_service/pages/budget_search_results.php"
+                                class="search-icon"> <i class="fa fa-search"></i> </a>
+                        </div>
+                    </div>
+
+                    <!-- Search for services  -->
+
+                    <h3 class="heading mt-5 text-center">Search By Specialization</h3>
+                    <div class="row mt-4 g-1 px-4 mb-5">
+                        <div class="col-md-2">
+                            <div class="card-inner p-3 d-flex flex-column align-items-center">
+                                <img src="../styling/icons/kitchen.png" width="50">
+                                <div class="text-center mg-text pt-1"> <span class="mg-text">Kitchen Renovation</span> </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
+                                    src="../styling/icons/bathtub.png" width="50">
+                                <div class="text-center mg-text pt-1"> <span class="mg-text">Bathroom Renovation</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
+                                    src="../styling/icons/house.png" width="50">
+                                <div class="text-center mg-text pt-1"> <span class="mg-text">Full House Renovation</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
+                                    src="../styling/icons/interior.png" width="50">
+                                <div class="text-center mg-text pt-1"> <span class="mg-text">Interior Design</span> </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
+                                    src="../styling/icons/plank.png" width="50">
+                                <div class="text-center mg-text pt-1"> <span class="mg-text">Flooring</span> </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
+                                    src="../styling/icons/paint.png" width="50">
+                                <div class="text-center mg-text pt-1"> <span class="mg-text">Painting</span> </div>
+                            </div>
                         </div>
                     </div>
                 </div>
