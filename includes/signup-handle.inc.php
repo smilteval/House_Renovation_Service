ab97cbfff285b5e78f93a16a2cadaeaee00b432a
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-05-06 20:06:25 -0400

    signup has required fields on all fields, can not submit empty form

--- includes/signup-handle.inc.php
+++ includes/signup-handle.inc.php
@@ -7,50 +7,74 @@
 
     // var_dump($_POST);
 
-    $error = [];
+    $errors = [];
 
     // echo "hi"; //test
     if(isset($_POST['signup'])){ //submit comes from attribute name in the button
         //Assume that you have a regular expression (regex)
-        $username = sanitizeString($_POST['username']);
-        $pwd = sanitizeString($_POST['password']);
-        $first_name = sanitizeString($_POST['first_name']);
-        $last_name = sanitizeString($_POST['last_name']);
-        $address = sanitizeString($_POST['address']);
-        $zipcode = sanitizeString($_POST['zipcode']);
-        $budget = sanitizeString($_POST['budget']);
+        if(empty($_POST['username'])){
+            $errors['username'] = "Missing Username";
+        }
+        if(empty($_POST['password'])){
+            $errors['password'] = "Missing Password";
+        }
+        if(empty($_POST['first_name'])){
+            $errors['first_name'] = "Missing First Name";
+        }
+        if(empty($_POST['last_name'])){
+            $errors['last_name'] = "Missing Last Name";
+        }
+        if(empty($_POST['address'])){
+            $errors['address'] = "Missing Address";
+        }
+        if(empty($_POST['zipcode'])){
+            $errors['zipcode'] = "Missing Zipcode";
+        }
+        if(empty($_POST['budget'])){
+            $errors['budget'] = "Missing Budget";
+        }
+        if(count($errors) == 0){ //if there are no errors, proceed
+    
+            $username = sanitizeString($_POST['username']);
+            $pwd = sanitizeString($_POST['password']);
+            $first_name = sanitizeString($_POST['first_name']);
+            $last_name = sanitizeString($_POST['last_name']);
+            $address = sanitizeString($_POST['address']);
+            $zipcode = sanitizeString($_POST['zipcode']);
+            $budget = sanitizeString($_POST['budget']);
 
 
-        // echo $username;
+            // echo $username;
 
-        $checkQ = "SELECT username FROM customer WHERE username = ?";
+            $checkQ = "SELECT username FROM customer WHERE username = ?";
 
-        //use prepare statements for these vars
-        $stmt = $conn->prepare($checkQ);
-        $stmt->bind_param("s",$username);
-        $stmt->execute();
+            //use prepare statements for these vars
+            $stmt = $conn->prepare($checkQ);
+            $stmt->bind_param("s",$username);
+            $stmt->execute();
 
-        //This is an object, it can tell you how many rows have returned from query
-        // $result = $stmt->get_result();
-        // var_dump($result());
+            //This is an object, it can tell you how many rows have returned from query
+            // $result = $stmt->get_result();
+            // var_dump($result());
 
-        if($stmt->get_result()->num_rows > 0){ //there is duplicate
-            $error['duplicate'] = "Account is taken";   
-        } else { //appending the account information into the db
+            if($stmt->get_result()->num_rows > 0){ //there is duplicate
+                $error['duplicate'] = "Account is taken";   
+            } else { //appending the account information into the db
 
-            //returns hashed string
-            $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT);
+                //returns hashed string
+                $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT);
 
-            $insertQ = "INSERT INTO customer (username, password, first_name, last_name, address, zipcode, budget)";
-            $insertQ .= "VALUES (?, ?, ?, ?, ?, ?, ?)"; //use ? to prevent SQL injection
+                $insertQ = "INSERT INTO customer (username, password, first_name, last_name, address, zipcode, budget)";
+                $insertQ .= "VALUES (?, ?, ?, ?, ?, ?, ?)"; //use ? to prevent SQL injection
 
-            $stmt2 = $conn->prepare($insertQ);
-            $stmt2->bind_param("sssssii",$username, $hashed_pwd, $first_name, $last_name, $address, $zipcode, $budget);
-            if(!$stmt2->execute()){
-                $error["insert"] = "We have a problem creating your account"; //can also use try catch block
-            } else { // no problems in creating the account
-                echo "Your account is created"; 
-                // ** Maybe redirect the user to welcome page or login? **
+                $stmt2 = $conn->prepare($insertQ);
+                $stmt2->bind_param("sssssii",$username, $hashed_pwd, $first_name, $last_name, $address, $zipcode, $budget);
+                if(!$stmt2->execute()){
+                    $error["insert"] = "We have a problem creating your account"; //can also use try catch block
+                } else { // no problems in creating the account
+                    echo "Your account is created"; 
+                    // ** Maybe redirect the user to welcome page or login? **
+                }
             }
         }
     }
--- pages/signup.php
+++ pages/signup.php
@@ -24,19 +24,47 @@ include "../includes/signup-handle.inc.php";
     <form action="signup.php" method="POST">
         <!-- use post method for sensitive data-->
         User: <input type="text" name="username" value="<?php echo $username ?? ''; ?>">
-        <br>
+    <br>
+        <small>
+            <?php echo (isset($errors['username'])) ? $errors['username'] : ''; ?>
+        </small>
+    <br>
         Password: <input type="password" name="password" value="<?php echo $password ?? ''; ?>">
-        <br>
+    <br>
+        <small>
+            <?php echo (isset($errors['password'])) ? $errors['password'] : ''; ?>
+        </small>
+    <br>
         First Name: <input type="text" name="first_name" value="<?php echo $first_name ?? ''; ?>">
-        <br>
+    <br>
+        <small>
+            <?php echo (isset($errors['first_name'])) ? $errors['first_name'] : ''; ?>
+        </small>
+    <br>
         Last Name: <input type="text" name="last_name" value="<?php echo $last_name ?? ''; ?>">
-        <br>
+    <br>
+        <small>
+            <?php echo (isset($errors['last_name'])) ? $errors['last_name'] : ''; ?>
+        </small>
+    <br>
         Address: <input type="text" name="address" value="<?php echo $address ?? ''; ?>">
-        <br>
+    <br>
+        <small>
+            <?php echo (isset($errors['address'])) ? $errors['address'] : ''; ?>
+        </small>
+    <br>
         Zip Code: <input type="number" name="zipcode" value="<?php echo $zipcode ?? ''; ?>">
-        <br>
+    <br>
+        <small>
+            <?php echo (isset($errors['zipcode'])) ? $errors['zipcode'] : ''; ?>
+        </small>
+    <br>
         Budget: <input type="number" name="budget" value="<?php echo $budget ?? ''; ?>">
-        <br>
+    <br>
+        <small>
+            <?php echo (isset($errors['budget'])) ? $errors['budget'] : ''; ?>
+        </small>
+    <br>
         <input type="submit" name="signup" value="Signup">
 
     </form>
